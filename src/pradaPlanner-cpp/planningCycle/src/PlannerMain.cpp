66a41c4f11e076db1c6f3658a0116984435b524e
AlexAntn <aleksander88@gmail.com>
2015-04-22 04:45:15 +0100

    [Planner] planningCycle, geoGround and goalCompiler are now in c++

---
+++ src/pradaPlanner-cpp/planningCycle/src/PlannerMain.cpp
@@ -0,0 +1,24 @@
+#include <yarp/os/Network.h>
+#include "PlannerModule.h"
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+
+    ResourceFinder rf;
+    rf.setVerbose(false);
+    rf.setDefaultContext("poeticon");    // overridden by --context
+    rf.setDefaultConfigFile("planner.ini");  // overridden by --from
+    rf.configure(argc, argv);
+
+
+
+    if(! yarp.checkNetwork() )
+    {
+        yError("YARP server not available!");
+        return -1; // EXIT_FAILURE
+    }
+
+    PlannerModule module;
+    return module.runModule(rf);
+}
