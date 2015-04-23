755cc9d5e3fbc8f155835526237eb9282c04ef5b
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2015-04-23 18:24:05 +0100

    [prada planner]: re-generated IDL files

---
+++ src/pradaPlanner-cpp/planningCycle/planningCycle_thrift.cmake
@@ -0,0 +1,5 @@
+## This is an automatically-generated file.
+## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
+
+set(headers include/planningCycle_IDLserver.h)
+set(sources src/planningCycle_IDLserver.cpp)
