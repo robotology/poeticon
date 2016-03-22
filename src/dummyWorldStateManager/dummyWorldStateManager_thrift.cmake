92145a19531f03d3932113764c74655a1b9d4e60
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2016-03-22 19:05:22 +0000

    add some missing IDL files

---
+++ src/dummyWorldStateManager/dummyWorldStateManager_thrift.cmake
@@ -0,0 +1,5 @@
+## This is an automatically-generated file.
+## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
+
+set(headers include/DummyWorldStateMgr_IDL.h)
+set(sources src/DummyWorldStateMgr_IDL.cpp)
