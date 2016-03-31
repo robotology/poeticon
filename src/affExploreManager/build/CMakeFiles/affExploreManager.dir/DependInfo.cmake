7eac1c748f96e904f2ab29c5056210e28957eb9d
Atabak Dehban <atabakd@gmail.com>
2016-03-31 15:44:04 +0100

    added tool_transform and info commands and updated hand instruction

---
+++ src/affExploreManager/build/CMakeFiles/affExploreManager.dir/DependInfo.cmake
@@ -0,0 +1,89 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/local/src/robot/OLD_poeticon/src/affExploreManager/src/main.cpp" "/usr/local/src/robot/OLD_poeticon/src/affExploreManager/build/CMakeFiles/affExploreManager.dir/src/main.cpp.o"
+  "/usr/local/src/robot/OLD_poeticon/src/affExploreManager/src/module.cpp" "/usr/local/src/robot/OLD_poeticon/src/affExploreManager/build/CMakeFiles/affExploreManager.dir/src/module.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/src/robot/icub-main/src/libraries/ctrlLib/include"
+  "/usr/local/src/robot/opencv3/build"
+  "/usr/local/src/robot/opencv3/include"
+  "/usr/local/src/robot/opencv3/include/opencv"
+  "/usr/local/src/robot/opencv3/modules/cudev/include"
+  "/usr/local/src/robot/opencv3/modules/core/include"
+  "/usr/local/src/robot/opencv3/modules/cudaarithm/include"
+  "/usr/local/src/robot/opencv3/modules/flann/include"
+  "/usr/local/src/robot/opencv_contrib/modules/hdf/include"
+  "/usr/local/src/robot/opencv3/modules/imgproc/include"
+  "/usr/local/src/robot/opencv3/modules/ml/include"
+  "/usr/local/src/robot/opencv_contrib/modules/reg/include"
+  "/usr/local/src/robot/opencv_contrib/modules/surface_matching/include"
+  "/usr/local/src/robot/opencv3/modules/video/include"
+  "/usr/local/src/robot/opencv3/modules/cudabgsegm/include"
+  "/usr/local/src/robot/opencv3/modules/cudafilters/include"
+  "/usr/local/src/robot/opencv3/modules/cudaimgproc/include"
+  "/usr/local/src/robot/opencv3/modules/cudawarping/include"
+  "/usr/local/src/robot/opencv_contrib/modules/dnn/include"
+  "/usr/local/src/robot/opencv_contrib/modules/fuzzy/include"
+  "/usr/local/src/robot/opencv3/modules/imgcodecs/include"
+  "/usr/local/src/robot/opencv3/modules/photo/include"
+  "/usr/local/src/robot/opencv3/modules/shape/include"
+  "/usr/local/src/robot/opencv3/modules/videoio/include"
+  "/usr/local/src/robot/opencv3/modules/cudacodec/include"
+  "/usr/local/src/robot/opencv3/modules/highgui/include"
+  "/usr/local/src/robot/opencv3/modules/objdetect/include"
+  "/usr/local/src/robot/opencv_contrib/modules/plot/include"
+  "/usr/local/src/robot/opencv3/modules/ts/include"
+  "/usr/local/src/robot/opencv_contrib/modules/xobjdetect/include"
+  "/usr/local/src/robot/opencv_contrib/modules/xphoto/include"
+  "/usr/local/src/robot/opencv_contrib/modules/bgsegm/include"
+  "/usr/local/src/robot/opencv_contrib/modules/bioinspired/include"
+  "/usr/local/src/robot/opencv_contrib/modules/dpm/include"
+  "/usr/local/src/robot/opencv_contrib/modules/face/include"
+  "/usr/local/src/robot/opencv3/modules/features2d/include"
+  "/usr/local/src/robot/opencv_contrib/modules/line_descriptor/include"
+  "/usr/local/src/robot/opencv_contrib/modules/saliency/include"
+  "/usr/local/src/robot/opencv_contrib/modules/text/include"
+  "/usr/local/src/robot/opencv3/modules/calib3d/include"
+  "/usr/local/src/robot/opencv_contrib/modules/ccalib/include"
+  "/usr/local/src/robot/opencv3/modules/cudafeatures2d/include"
+  "/usr/local/src/robot/opencv3/modules/cudalegacy/include"
+  "/usr/local/src/robot/opencv3/modules/cudaobjdetect/include"
+  "/usr/local/src/robot/opencv3/modules/cudaoptflow/include"
+  "/usr/local/src/robot/opencv3/modules/cudastereo/include"
+  "/usr/local/src/robot/opencv_contrib/modules/datasets/include"
+  "/usr/local/src/robot/opencv_contrib/modules/rgbd/include"
+  "/usr/local/src/robot/opencv_contrib/modules/stereo/include"
+  "/usr/local/src/robot/opencv_contrib/modules/structured_light/include"
+  "/usr/local/src/robot/opencv3/modules/superres/include"
+  "/usr/local/src/robot/opencv_contrib/modules/tracking/include"
+  "/usr/local/src/robot/opencv3/modules/videostab/include"
+  "/usr/local/src/robot/opencv_contrib/modules/xfeatures2d/include"
+  "/usr/local/src/robot/opencv_contrib/modules/ximgproc/include"
+  "/usr/local/src/robot/opencv_contrib/modules/aruco/include"
+  "/usr/local/src/robot/opencv_contrib/modules/optflow/include"
+  "/usr/local/src/robot/opencv3/modules/stitching/include"
+  "/usr/local/src/robot/opencv_contrib/modules/matlab/include"
+  "/usr/local/src/robot/yarp/build/generated_include"
+  "/usr/local/src/robot/yarp/src/libYARP_OS/include"
+  "/usr/local/src/robot/yarp/src/libYARP_sig/include"
+  "/usr/local/src/robot/yarp/src/libYARP_math/include"
+  "/usr/local/src/robot/yarp/src/libYARP_dev/include"
+  "/usr/local/src/robot/yarp/src/libYARP_name/include"
+  "/usr/local/src/robot/yarp/src/libYARP_manager/include"
+  "/usr/local/src/robot/yarp/src/libYARP_logger/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
